f288281b1f2b9a752e6ba7318d5378fa126120e0
Zackrey Bichsel <biksel09@hotmail.com>
2023-06-30 16:39:02 -0500

    Updated starter code to include psuedocode

--- script.js
+++ script.js
@@ -12,3 +12,25 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+function generatePassword() {
+    // prompt how many characters
+    // var for charcters
+
+    // What types of charctyers
+    // special, number, upper, lower
+    // window.confirm
+
+    // Use answers to make password
+    // vars that include all possible charcters
+    // seperate vars for each type, arrays
+    // example var upperLetters = ["A", "B", "C"]
+
+    // take all true arrays and concat into new array -loop
+    // choose random chars from arr based on user input
+    // var push into new array
+    // var password = turn array into string .toString()
+
+    // return password;
+}
