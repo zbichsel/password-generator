c8bc4db49b98210baa81e6c2ad69833746e329ce
Zackrey Bichsel <biksel09@hotmail.com>
2023-07-01 12:38:44 -0500

    added variables and if statements and console logged to test if info was being recorded

--- script.js
+++ script.js
@@ -16,14 +16,57 @@ generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
     // prompt how many characters
-    // var for charcters
+    var passLength = this.prompt("How many characters would you like your password to be?");
+    console.log(passLength);
+    var characters = '';
 
+    var lowercaseLetters = this.confirm("Would you like the password to include lowercase letters?");
+    console.log(lowercaseLetters);
+    var lowercaseABC = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+    var uppercaseLetters = this.confirm("Would you like this password to include uppercase letters?");
+    console.log(uppercaseLetters);
+    var uppercaseABC = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    
+
+    var specialConfirm = this.confirm("Would you like this password to include special characters?");
+    console.log(specialConfirm);
+    var specialChars = ['`', '~', '!', '@', '#', '$', '%', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', ',', '<', '.', '>', '/', '?', '[', '{', ']', '}', '|'];
+
+    var numberConfirm = this.confirm("Would you like this password to include numbers?");
+    console.log(numberConfirm);
+    var nummerChars = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+
+    var passwordPrompts = (passLength + ", " + lowercaseLetters + ", " + uppercaseLetters + ", " + specialConfirm + ", " + numberConfirm);
+    console.log(passwordPrompts);
+
+    var chosenChars = [];
+
+    if (passwordPrompts) {
+        chosenChars = lowercaseABC.concat(chosenChars);
+    }
+
+    if (uppercaseLetters) {
+        chosenChars = uppercaseABC.concat(chosenChars);
+    }
+
+    if (specialConfirm) {
+        chosenChars = specialChars.concat(chosenChars);
+    }
+
+    if (numberConfirm) {
+        chosenChars = nummerChars.concat(chosenChars);
+    }
+
+    console.log(chosenChars);
+    // var for charcters
     // What types of charctyers
     // special, number, upper, lower
     // window.confirm
 
     // Use answers to make password
-    // vars that include all possible charcters
+    // vars that include all possible charcters 
     // seperate vars for each type, arrays
     // example var upperLetters = ["A", "B", "C"]
 
@@ -33,4 +76,6 @@ function generatePassword() {
     // var password = turn array into string .toString()
 
     // return password;
+
 }
+writePassword();
