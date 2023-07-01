e6564566d1ec57a7d9035711073902424d1f3d00
Zackrey Bichsel <biksel09@hotmail.com>
2023-07-01 16:59:29 -0500

    updated logic on for-loop

--- script.js
+++ script.js
@@ -16,56 +16,58 @@ generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
     // prompt how many characters
-    var passLength = this.prompt("How many characters would you like your password to be?");
-    console.log(passLength);
+    var passLength = prompt("How many characters would you like your password to be?");
     var characters = '';
 
-    var lowercaseLetters = this.confirm("Would you like the password to include lowercase letters?");
-    console.log(lowercaseLetters);
+    var lowercaseLetters = confirm("Would you like the password to include lowercase letters?");
     var lowercaseABC = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-    var uppercaseLetters = this.confirm("Would you like this password to include uppercase letters?");
-    console.log(uppercaseLetters);
+    var uppercaseLetters = confirm("Would you like this password to include uppercase letters?");
     var uppercaseABC = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
     
 
-    var specialConfirm = this.confirm("Would you like this password to include special characters?");
-    console.log(specialConfirm);
+    var specialConfirm = confirm("Would you like this password to include special characters?");
     var specialChars = ['`', '~', '!', '@', '#', '$', '%', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', ',', '<', '.', '>', '/', '?', '[', '{', ']', '}', '|'];
 
-    var numberConfirm = this.confirm("Would you like this password to include numbers?");
-    console.log(numberConfirm);
+    var numberConfirm = confirm("Would you like this password to include numbers?");
     var nummerChars = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 
 
-    var passwordPrompts = (lowercaseLetters + ", " + uppercaseLetters + ", " + specialConfirm + ", " + numberConfirm);
+    var passwordPrompts = (passLength + ", " + lowercaseLetters + ", " + uppercaseLetters + ", " + specialConfirm + ", " + numberConfirm);
     console.log(passwordPrompts);
+
+    if (lowercaseLetters === false && uppercaseLetters === false && specialConfirm === false && numberConfirm === false) {
+        alert("Please choose at least one of the character types to proceed!");
+    }
 
     var chosenChars = [];
 
-    if (passwordPrompts) {
-        chosenChars = lowercaseABC.concat(chosenChars);
+    if (lowercaseLetters) {
+        chosenChars = chosenChars.concat(lowercaseABC);
     }
 
     if (uppercaseLetters) {
-        chosenChars = uppercaseABC.concat(chosenChars);
+        chosenChars = chosenChars.concat(uppercaseABC);
     }
 
     if (specialConfirm) {
-        chosenChars = specialChars.concat(chosenChars);
+        chosenChars = chosenChars.concat(specialChars);
     }
 
     if (numberConfirm) {
-        chosenChars = nummerChars.concat(chosenChars);
+        chosenChars = chosenChars.concat(nummerChars);
     }
-
     console.log(chosenChars);
 
-    var finalPass;
-    for (var i = 0; i <= passLength; i++) {
-        finalPass = [Math.floor(Math.random() * chosenChars.length)]
+    var finalPass = '';
+    for (var i = 0; i < passLength; i++) {
+        var randomPass = chosenChars[Math.floor(Math.random() * chosenChars.length)];
+
+        finalPass += chosenChars[randomPass];
     }
 
+
+    console.log(finalPass);
     return finalPass;
     // var for charcters
     // What types of charctyers
