9b4f89012fd8986c2242e230f5f65d1bc4ca4cf5
Zackrey Bichsel <biksel09@hotmail.com>
2023-07-01 19:58:51 -0500

    updated notes

--- script.js
+++ script.js
@@ -12,10 +12,7 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 // What types of charcters
-// special, number, upper, lower
-// vars that include all possible charcters 
-// seperate vars for each type, arrays
-// example var upperLetters = ["A", "B", "C"]
+// special, number, upper, lower as arrays
 var lowercaseABC = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var uppercaseABC = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var specialChars = ['`', '~', '!', '@', '#', '$', '%', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', ',', '<', '.', '>', '/', '?', '[', '{', ']', '}', '|'];
